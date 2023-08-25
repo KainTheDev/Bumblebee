0b374bc589230f4f6efdcdf997144468ac06e434
KainTheDev <89621266+KainTheDev@users.noreply.github.com>
2023-08-25 13:10:13 +0000

    2 changes in index and ready file

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,7 @@ const client = require("../index");
 
 const {ActivityType} = require("discord.js")
   client.once('ready', () => {
+    console.log(client.user.tag)
     console.log('Bot - status: ✅');
     client.user.setPresence({
   activities: [{ name: `Jazz`, type: ActivityType.Playing }],
